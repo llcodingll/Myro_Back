b6aec820fe1f70100056fb37ae1b7b04a510376c
llcodingll <ll.o.llwhkr@gmail.com>
2025-04-23 14:47:20 +0900

    rename: change to TagController

--- src/main/java/com/lloll/myro/domain/schedule/api/TagController.java
+++ src/main/java/com/lloll/myro/domain/schedule/api/TagController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tags")
 @RequiredArgsConstructor
-public class ScheduleTagController {
+public class TagController {
 
     private final TagRepository tagRepository;
 
