ee88453b737004216f12148989f24700f6ddf4fd
llcodingll <ll.o.llwhkr@gmail.com>
2025-05-05 23:02:33 +0900

    feat: mapping tag

--- src/main/java/com/lloll/myro/domain/schedule/dao/TagRepository.java
+++ src/main/java/com/lloll/myro/domain/schedule/dao/TagRepository.java
@@ -1,7 +1,9 @@
 package com.lloll.myro.domain.schedule.dao;
 
 import com.lloll.myro.domain.schedule.domain.Tag;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
 }
